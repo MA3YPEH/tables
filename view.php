65f3296abb376ecaeaadc4592751b57ed4408797
MA3YPEH <MA3YP.EH@yandex.ru>
2024-08-16 10:48:57 +0300

    Error

--- view.php
+++ view.php
@@ -63,7 +63,7 @@ $PAGE->set_context($modulecontext);
 $PAGE->requires->jquery();
 $PAGE->requires->js(new moodle_url($CFG->wwwroot . '/mod/tables/amd/src/connect_to_websocket.js?v=3.0'));
 $PAGE->requires->js(new moodle_url($CFG->wwwroot . '/mod/tables/amd/src/update_data.js?v=5.3'));
-$PAGE->requires->js(new moodle_url($CFG->wwwroot . '/mod/tables/amd/src/interact_resize.js?v=2.0'));
+$PAGE->requires->js(new moodle_url($CFG->wwwroot . '/mod/tables/amd/src/interact_resize.js?v=2.1'));
 $PAGE->requires->js(new moodle_url($CFG->wwwroot . '/mod/tables/amd/src/attach_cells.js?v=2.5'));
 
 $roles = get_default_enrol_roles($modulecontext);
